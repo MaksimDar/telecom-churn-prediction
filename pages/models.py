541a78f1fb2f27d03f48dfe92dd8ebd42edea9b9
MaksimDar <dovhusha.m@gmail.com>
2026-09-06 17:44:26 +0200

    All 5 models are built

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title('Розроблені моделі')
+language = st.sidebar.selectbox(
+    "Мова / Language",
+    ["Українська", "English"]
+)
+
+if language == "Українська":
+    st.title('Розроблені моделі')
+else:
+    st.title('Developed models')
